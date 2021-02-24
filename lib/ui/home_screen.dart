61595ad4eed190f98367317598f63ec4fc7dd8ae
devadilson <didasoueudida@gmail.com>
2021-02-24 17:37:49 -0300

    Home Screen

---
+++ lib/ui/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        child: Padding(
+          padding: EdgeInsets.all(50.0),
+          child: Image.asset(
+            'assets/images/github.png',
+            height: 120.0,
+          ),
+        ),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(120.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
