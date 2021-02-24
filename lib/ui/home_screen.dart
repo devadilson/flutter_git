41d1345d22c958fe02af468fc88bc5496f3939aa
devadilson <didasoueudida@gmail.com>
2021-02-24 18:32:38 -0300

    Home Screen Update

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,24 +1,67 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.maxFinite,
-        child: Padding(
-          padding: EdgeInsets.all(50.0),
-          child: Image.asset(
-            'assets/images/github.png',
-            height: 120.0,
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.maxFinite,
+            child: Padding(
+              padding: EdgeInsets.all(50.0),
+              child: Image.asset(
+                'assets/images/github.png',
+                height: 120.0,
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(120.0),
+              ),
+            ),
           ),
-        ),
-        decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(120.0),
+          Padding(
+            padding: EdgeInsets.all(12.0),
+            child: Column(
+              children: <Widget>[
+                Container(height: 30.0),
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 1.0)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 1.0)),
+                      hintText: 'username',
+                      labelText: 'your username git',
+                      labelStyle: TextStyle(color: Colors.black)),
+                ),
+                Container(height: 10.0),
+                ButtonTheme(
+                  height: 40.0,
+                  minWidth: double.maxFinite,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: RaisedButton(
+                    onPressed: () {},
+                    textColor: Colors.white,
+                    color: Colors.black,
+                    child: Text(
+                      'Search Git',
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
